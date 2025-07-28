9073696b82f894ae35366420550874ecd7b6aa88
rahsharma01 <rah.sharma01@gmail.com>
2025-07-28 21:50:07 +0530

    modified sh file

--- runqemu.sh
+++ runqemu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Script to run QEMU for buildroot as the default configuration qemu_aarch64_virt_defconfig
 #Host forwarding: Host Port 10022 ->> QEMU Port 22 
-#Author: Siddhant Jajoo.
+#Author: Rahul Sharma.
 
 
 qemu-system-aarch64 \
